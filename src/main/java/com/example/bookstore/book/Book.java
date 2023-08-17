04ea2ea2a23a1f40d20aa1a580669b6912cf8f86
LiS2Lim <s2alua.fu@gmail.com>
2023-08-17 10:17:28 +0900

    Make Entity of book

---
+++ src/main/java/com/example/bookstore/book/Book.java
@@ -0,0 +1,40 @@
+package com.example.bookstore.book;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 200, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ColumnDefault("0")
+    private int price;
+
+    @Column
+    private String url;
+
+    @Column
+    private LocalDate publishedDate;
+
+    @Column
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+}
