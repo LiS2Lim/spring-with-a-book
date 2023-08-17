2077a7296e4c9f754d469ae3c3cea98bc428307e
LiS2Lim <s2alua.fu@gmail.com>
2023-08-17 10:24:14 +0900

    Make DB connection information in application.properties

--- src/main/java/com/example/bookstore/book/Book.java
+++ src/main/java/com/example/bookstore/book/Book.java
@@ -11,8 +11,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,5 +38,4 @@ public class Book {
     @Column
     @CreatedDate
     private LocalDateTime createdDate;
-
 }
